f6685ddbd0dc1387b6f2f730b63d4b9b153087e7
vadim-sobinin <vadimsobinin@yandex.ru>
2022-09-25 23:42:28 +0300

    Add card template

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
     <div className="App clear">
       <header className="d-flex justify-between align-center p-40">
        <div className="d-flex align-center">
-        <img src="/img/logo.png" width={40} height={40}/>
+        <img src="/img/logo.png" alt="logo" width={40} height={40}/>
         <div className="headerInfo">
           <h3 className="text-uppercase">React Sneakers</h3>
           <p>Shop for the best sneakers</p>
@@ -12,23 +12,35 @@ function App() {
        </div>
         <ul className="d-flex">
           <li className="mr-30">
-            <img src="/img/cart.svg" className="mr-10" width={18} height={17}/>
+            <img src="/img/cart.svg" className="mr-10" alt="cart" width={18} height={17}/>
             <span>20$</span>
           </li>
           <li className="mr-30">
-            <img src="/img/heart.svg" width={21} height={19}/>
+            <img src="/img/heart.svg" alt="liked" width={21} height={19}/>
           </li>
           <li> 
-            <img src="/img/user.svg" width={20} height={20}/>
+            <img src="/img/user.svg" alt="user-profile" width={20} height={20}/>
           </li>
         </ul>
       </header>
 
       <div className="content p-40">
-        <h1>All sneakers</h1>
+        <h1 className="mb-40">All sneakers</h1>
         
 
-      ....
+      <div className="card">
+        <img width={133} height={113} src="/img/sneakers/sneakers1.jpg" alt="sneakers" />
+        <h5>Male Sneakers <br />Nike Blazer Mid Suede</h5>
+        <div className="cardBottom d-flex justify-between align-center">
+          <div className="d-flex flex-column ">
+            <span>Price:</span>
+            <b>49.99$</b>
+          </div>
+          <button className="button">
+            <img src="/img/plus.svg" alt="add" width={11} height={11} />
+          </button>
+        </div>
+      </div>
 
 
 
